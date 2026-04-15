61b8cf42fa3e65f69bffa1cf72c61820d4731519
Charath H J <charath11@gmail.com>
2026-04-15 12:05:49 +0530

    Add: combination-target-sum - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum/submission-6.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack (result, list, nums, target, 0);
+
+        return result;
+    }
+
+    public void backtrack (List<List<Integer>> result, List<Integer> list, int[] nums, int target, int start) {
+        
+        if (target == 0) {
+            result.add (new ArrayList<> (list));
+            return;
+        } 
+
+        if (target < 0) return;
+
+        for (int i = start; i < nums.length; i++) {
+            list.add (nums[i]);
+            backtrack (result, list, nums, target - nums[i], i);
+            list.remove (list.size() - 1);
+        }
+    }
+}
