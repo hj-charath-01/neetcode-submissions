172b49478b91c140b0c52113cd84c085661d388b
Charath H J <charath11@gmail.com>
2026-04-18 11:44:52 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    boolean[][] dp;
+
+    public List<List<String>> partition(String s) {
+        
+        int n = s.length();
+        List<List<String>> result = new ArrayList<>();
+        dp = new boolean [n][n];
+
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                dp [i][i + l -1] = s.charAt (i) == s.charAt (i + l - 1) && (i + 1 > (i + l -2) || dp [i + 1][i + l - 2]);
+            }
+        }
+
+        dfs (result, new ArrayList<>(), s, 0);
+
+        return result;
+    }
+
+    public void dfs (List<List<String>> result, List<String> list, String s, int start) {
+
+        if (start >= s.length()) {
+            result.add (new ArrayList<> (list));
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            if (dp[start][i]) {
+                list.add (s.substring (start, i + 1));
+                dfs (result, list, s, i + 1);
+                list.remove (list.size() - 1);
+            }
+        }
+    }
+}
