83e75d5e8ae712a64909b840f3f38ff4a776333a
Charath H J <charath11@gmail.com>
2026-04-30 03:00:59 +0530

    Add: pow-x-n - submission-6

---
+++ Data Structures & Algorithms/pow-x-n/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long num = n;
+
+        if (num < 0) {
+            x = 1/x;
+            num = -num;
+        }
+
+        double result = 1.0;
+        while (num > 0) {
+            if (num % 2 == 1) result *= x;
+            x *= x;
+            num /= 2;
+        }
+
+        return result;
+    }
+}
