db33e123790c48a26ea69e8f2393148a92524297
Charath H J <charath11@gmail.com>
2026-05-05 23:53:52 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.java
@@ -0,0 +1,54 @@
+public class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean end = false;
+}
+
+class WordDictionary {
+    TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+
+            if (curr.children[i] == null) {
+                curr.children[i] = new TrieNode();
+            }
+
+            curr = curr.children[i];
+        }
+
+        curr.end = true;
+    }
+
+    public boolean search(String word) {
+        return dfs (word, 0, root);
+    }
+
+    private boolean dfs (String word, int j, TrieNode root) {
+        TrieNode curr = root;
+
+        for (int i = j; i < word.length(); i++) {
+            char c = word.charAt (i);
+
+            if (c == '.') {
+                for (TrieNode child : curr.children) {
+                    if (child != null && dfs (word, i + 1, child)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            } else {
+                if (curr.children[c - 'a'] == null) return false;
+                curr = curr.children[c - 'a'];
+            }
+        }
+
+        return curr.end;
+    }
+}
