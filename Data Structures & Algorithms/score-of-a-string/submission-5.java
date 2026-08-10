c67220f885eaa246f174edab7f7dbf4cb3bf3e75
Charath H J <charath11@gmail.com>
2026-08-10 15:21:19 +0530

    Update: score-of-a-string - submission-5

--- Data Structures & Algorithms/score-of-a-string/submission-5.java
+++ Data Structures & Algorithms/score-of-a-string/submission-5.java
@@ -1,11 +1,10 @@
 class Solution {
     public int scoreOfString(String s) {
-        int sum = 0;
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            sum += Math.abs ((int) s.charAt (i) - (int) s.charAt (i + 1));
+        int score = 0;
+        for (int i = 1; i < s.length(); i++) {
+            score += Math.abs ((int)s.charAt(i) - (int)s.charAt(i - 1));
         }
 
-        return sum;
+        return score;
     }
 }
