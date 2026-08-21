3d12a04fdc97289002d26c7ed37acc9f16639628
Charath H J <charath11@gmail.com>
2026-08-22 01:41:38 +0530

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.java
@@ -0,0 +1,49 @@
+class Trie{
+    Trie[] child;
+    boolean end;
+
+    public Trie() {
+        this.child = new Trie[26];
+        this.end = false;
+    }
+}
+
+class PrefixTree {
+    public Trie root;
+
+    public PrefixTree() {
+        root = new Trie();
+    }
+
+    public void insert(String word) {
+        Trie curr = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if(curr.child[idx] == null) {
+                curr.child[idx] = new Trie();
+            }
+            curr = curr.child[idx];
+        }
+        curr.end = true;
+    }
+
+    public boolean search(String word) {
+        Trie curr = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if(curr.child[idx] == null) return false;
+            curr = curr.child[idx];
+        }
+        return curr.end;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie curr = root;
+        for (char c : prefix.toCharArray()) {
+            int idx = c - 'a';
+            if(curr.child[idx] == null) return false;
+            curr = curr.child[idx];
+        }
+        return true;
+    }
+}
